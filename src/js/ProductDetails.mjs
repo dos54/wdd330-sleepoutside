55d77787129a7a1cbce9e77e26ca978613e2e0bc
COMICP <binettejaden@gmail.com>
2025-01-19 00:36:01 -0500

    remove items from cart

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -3,7 +3,7 @@ import { setLocalStorage } from "./utils.mjs";
 function productDetailsTemplate(product) {
     return `
     <section class="product-detail">
-        <h3>${ product.Brand.Name}</h3>
+        <h3>${product.Brand.Name}</h3>
 
         <h2 class="divider">${ product.NameWithoutBrand}</h2>
 
--- src/js/cart.js
+++ src/js/cart.js
@@ -5,7 +5,35 @@ function renderCartContents() {
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
+//calls items from cart then compares them to the deleted item, first matching ID is removed then cart is overwritten
+function parseAndRemoveByID(itemID){
+  const cartItems = getLocalStorage("so-cart");
+  let itemRemoved = false;
+  const updatedCart = [];
+  cartItems.forEach(item => {
+    if (item.Id === itemID && itemRemoved === false ){
+      itemRemoved = true;
+    }
+    else{
+      updatedCart.push(item)
+    }
+  });
+  return updatedCart;
+};
+//calls remove function and updates cart
+function removeItemsFromCart(itemID){
+  console.log('removing ' + itemID);
+  let newCart = parseAndRemoveByID(itemID);
+  localStorage.setItem('so-cart', JSON.stringify(newCart));
+  console.log('removed');
+};
 
+function buildCartControls(){
+  const cartItems = getLocalStorage("so-cart");
+  cartItems.map((item) => document.getElementById(`removeItem${item.Id}`)
+  .addEventListener('click', ()=> {removeItemsFromCart(item.Id)} ));
+
+}
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
@@ -20,9 +48,11 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+  <button id="removeItem${item.Id}" >Remove Item</button>
 </li>`;
 
   return newItem;
 }
 
 renderCartContents();
+buildCartControls();
