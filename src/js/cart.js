25d2b345ca095b433330a35f7d1722c7c62229aa
Steven Thomas <stevencthomas54@gmail.com>
2025-01-21 19:00:45 -0900

    Tons o stuff done

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,13 +1,17 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
+
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  const productList = document.querySelector(".product-list");
   if (!cartItems.length) {
     document.querySelector(".product-list").innerHTML =
-      "<p>Your cart is empty.</p>";
+    "<p>Your cart is empty.</p>";
+    return;
   }
+  
+  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+  document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
 //calls items from cart then compares them to the deleted item, first matching ID is removed then cart is overwritten
 function parseAndRemoveByID(itemID){
@@ -52,11 +56,12 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
-  <button id="removeItem${item.Id}" >Remove Item</button>
+  <button id="removeItem${item.Id}" onClick="">Remove Item</button>
 </li>`;
 
   return newItem;
 }
+
 
 function getTotalPrice(localStorageKey){
   const cartItems = getLocalStorage(localStorageKey);
@@ -79,5 +84,6 @@ function showTotalPrice(localStorageKey,parentElementId,elementId, classHide, cl
   }
 }
 
+loadHeaderFooter();
 renderCartContents();
 buildCartControls();
--- src/js/product.js
+++ src/js/product.js
@@ -1,12 +1,14 @@
-import { getParam } from "./utils.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
+
 
 const productId = getParam("product");
 const dataSource = new ProductData("tents");
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
+loadHeaderFooter();
 
 // add to cart button event handler
 // async function addToCartHandler(e) {
