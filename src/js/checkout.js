fef64ec59c64e4f327f635c5220587858c901e26
Jared Reynolds <icedraggon234@gmail.com>
2025-01-29 01:24:21 -0700

    Finished team activity

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -8,12 +8,19 @@ import {
 const dataLoad = ["cart", getLocalStorage, "so-cart", "cartNumberStyle"];
 loadHeaderFooter(dataLoad, numberOfItemsFn);
 
-const checkoutProcess = new CheckoutProcess("so-cart", "#order-summary")
-checkoutProcess.init()
+const checkoutProcess = new CheckoutProcess("so-cart", "#order-summary");
+checkoutProcess.init();
 
-document.querySelector("#zip").addEventListener("blur", checkoutProcess.calculateOrderTotal.bind(checkoutProcess));
-document.querySelector("form button.submit").addEventListener("click", (event) => {
-    event.preventDefault()
+document
+  .querySelector("#zip")
+  .addEventListener(
+    "blur",
+    checkoutProcess.calculateOrderTotal.bind(checkoutProcess),
+  );
+document
+  .querySelector("form button.submit")
+  .addEventListener("click", (event) => {
+    event.preventDefault();
 
     checkoutProcess.checkout();
-})
+  });
